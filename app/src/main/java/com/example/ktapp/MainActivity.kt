1f20dac71049e40fbb569aab44f43b317a360535
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2021-12-30 15:39:17 +0530

    Initial Commitment

--- app/src/main/java/com/example/ktapp/MainActivity.kt
+++ app/src/main/java/com/example/ktapp/MainActivity.kt
@@ -3,30 +3,85 @@ package com.example.ktapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ktapp.ui.theme.KtAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KtAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            ScrollableColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+            ) {
+               Image(painter = painterResource(id = R.drawable.happy_meal_small), contentDescription = null,
+                   modifier = Modifier.height(300.dp).fillMaxWidth(), contentScale = ContentScale.Crop)
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Happy Meals",
+                        style = TextStyle(fontSize = 26.sp)
+                        )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "800 Calories",
+                        style = TextStyle(fontSize = 17.sp))
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "$5.99", color = Color.Green,
+                        style = TextStyle(fontSize = 17.sp))
                 }
             }
         }
-    }
+        }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+@Composable
+public fun ImageFromResource(
+    resId: Int
+){
+    Image(
+        painter = painterResource(resId),
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+@Composable
+fun ScrollableColumn(
+    modifier: Modifier = Modifier,
+    scrollState: ScrollState = rememberScrollState(0),
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    reverseScrollDirection: Boolean = false,
+    isScrollEnabled: Boolean = true,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(
+                scrollState,
+                isScrollEnabled,
+                reverseScrolling = reverseScrollDirection
+            )
+            .padding(contentPadding),
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
 }
 
 @Preview(showBackground = true)
